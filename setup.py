48347b9a3f1558dcb7531c19a6e14a169ca31dad
JannisNe <necker@physik.hu-berlin.de>
2022-07-30 13:30:55 +0200

    make repo installable

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name="asassn_nu",
+        version="0.1",
+        author="Jannis Necker",
+        author_email="jannis.necker@gmail.com",
+        description="Code to produce results published  in https://arxiv.org/abs/2204.00500",
+        license="MIT",
+        keywords="neutrino astronomy high-energy optical",
+        url="https://github.com/JannisNe/nu_stat_for_asassn_paper",
+        project_urls={
+            "Bug Tracker": "https://github.com/JannisNe/nu_stat_for_asassn_paper/issues",
+        },
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.10",
+        ],
+        python_requires='3.10',
+        install_requires=[
+            "matplotlib==3.5.1",
+            "pandas==1.4.1",
+            "jupyterlab==3.2.9",
+            "seaborn==0.11.2",
+            "numpy==1.22.2",
+            "astropy==5.0.4",
+            "scipy==1.8.0",
+            "tqdm==4.64.0 ",
+            "flarestack==2.4.4"
+        ],
+        package_data={'timewise': [
+            'wise_flux_conversion_correction.dat'
+        ]}
+    )
