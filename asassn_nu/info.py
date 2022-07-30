d7f000608de48da65a24d6039e5a2a4f28c060f8
JannisNe <necker@physik.hu-berlin.de>
2022-07-30 15:37:18 +0200

    create directories

--- asassn_nu/info.py
+++ asassn_nu/info.py
@@ -5,4 +5,10 @@ output_dir = os.getenv("ASASSN_NU_OUTPUT", "../../")
 output_folder_figures = os.path.join(output_dir, "figures")
 output_folder_latex = os.path.join(output_dir, "latex")
 
-data_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "../"))
+data_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "../../data"))
+
+
+def make_directories():
+    for d in [output_dir, output_folder_latex, output_folder_figures]:
+        if not os.path.isdir(d):
+            os.mkdir(d)
--- asassn_nu/run_all.py
+++ asassn_nu/run_all.py
@@ -1,3 +1,4 @@
+from asassn_nu.info import make_directories
 from asassn_nu.observation_stats import make_obs_plots
 from asassn_nu.fermi_matches_lightcurves import plot_fermi_matches_lightcurves
 from candidates import make_candidates_plot, make_at2020rng_plot
@@ -7,6 +8,7 @@ from missed_candidates import plot_missed_candidates
 
 
 if __name__ == '__main__':
+    make_directories()
     make_obs_plots()
     plot_fermi_matches_lightcurves()
     make_candidates_plot()
