201f42a9d901d51e87451201309f5ce4141c27f3
JannisNe <necker@physik.hu-berlin.de>
2022-08-01 19:21:45 +0200

    reduce python version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 3.10",
         ],
-        python_requires='>=3.10',
+        python_requires='>=3.9',
         install_requires=[
             "matplotlib==3.5.1",
             "pandas==1.4.1",
